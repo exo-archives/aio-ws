f20c3be4932f38d9cec167df1e12ad6283ca730f
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2007-09-17 11:44:21 +0000

    RestEJBConnector version for EJB 2.1

---
+++ rest/ejb/src/main/java/org/exoplatform/services/rest/ejb21/RestEJBConnectorLocalHome.java
@@ -0,0 +1,18 @@
+/***************************************************************************
+ * Copyright 2001-2007 The eXo Platform SARL         All rights reserved.  *
+ * Please look at license.txt in info directory for more license detail.   *
+ **************************************************************************/
+package org.exoplatform.services.rest.ejb21;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBLocalHome;
+
+/**
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ * @version $Id: $
+ */
+public interface RestEJBConnectorLocalHome extends EJBLocalHome {
+  
+  public RestEJBConnectorLocal create() throws CreateException;
+
+}
