0c762184500e9fdd62a5921cd6dbbb6ddddf5dfe
Volodymyk Krasnokiv <volodymyr.krasnikov@exoplatform.com.ua>
2008-02-25 08:36:16 +0000

    listener for starting StandaloneContainer

---
+++ rest/core/src/main/java/org/exoplatform/services/rest/servlet/WebConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.services.rest.servlet;
+/**
+ * Created by The eXo Platform SAS        .
+ * @author <a href="mailto:gennady.azarenkov@exoplatform.com">Gennady Azarenkov</a>
+ * @version $Id: WebConstants.java 5800 2006-05-28 18:03:31Z geaz $
+ */
+
+public interface WebConstants {
+  public static final String APP_CONTEXT = "org.exoplatform.frameworks.web.appContext";
+  public static final String REPOSITORY_JNDI_NAME = "org.exoplatform.frameworks.web.repositoryJNDIName";
+  public static final String EXO_CONTAINER = "org.exoplatform.frameworks.web.eXoContainer";
+  public static final String CATALOG_NAME = "org.exoplatform.frameworks.web.catalogName";
+}
