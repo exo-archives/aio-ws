babb38d55bc1fb89c38b37a4c0609d16b325291c
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2008-07-07 07:51:29 +0000

    WS-43 : new code for rest ejb connector

---
+++ rest/ejb/connector2_1/modules/connector/src/main/java/org/exoplatform/ws/rest/ejbconnector21/RestEJBConnectorLocal.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+
+package org.exoplatform.ws.rest.ejbconnector21;
+
+import java.io.IOException;
+
+import javax.ejb.EJBLocalObject;
+
+import org.exoplatform.common.transport.SerialRequest;
+import org.exoplatform.common.transport.SerialResponse;
+
+/**
+ * Work with REST service through EJB.
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ * @version $Id: $
+ */
+public interface RestEJBConnectorLocal extends EJBLocalObject {
+
+  SerialResponse service(SerialRequest request) throws IOException;
+  
+}
