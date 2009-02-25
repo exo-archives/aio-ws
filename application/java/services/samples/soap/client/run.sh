a742265677db9a52813b2e0ac214e7c20b5431ed
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2009-02-25 09:28:34 +0000

    WS-145 Move to CXF 2.1.4

--- application/java/services/samples/soap/client/run.sh
+++ application/java/services/samples/soap/client/run.sh
@@ -7,20 +7,20 @@ if ( test -z "$EXO_SERVER" ); then
 fi 
 
 java -cp ./target/exo.ws.application.soap.cxf.samples.client-2.0.jar:\
-$EXO_SERVER/lib/cxf-api-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-transports-http-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-tools-wsdlto-core-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-tools-java2ws-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-frontend-jaxws-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-common-utilities-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-core-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-bindings-soap-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-tools-common-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-databinding-jaxb-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-transports-http-jetty-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-frontend-simple-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-ws-addr-2.1.2.jar:\
-$EXO_SERVER/lib/cxf-rt-ws-addr-2.1.2.jar:\
+$EXO_SERVER/lib/cxf-api-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-transports-http-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-tools-wsdlto-core-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-tools-java2ws-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-frontend-jaxws-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-common-utilities-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-core-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-bindings-soap-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-tools-common-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-databinding-jaxb-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-transports-http-jetty-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-frontend-simple-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-ws-addr-2.1.4.jar:\
+$EXO_SERVER/lib/cxf-rt-ws-addr-2.1.4.jar:\
 $EXO_SERVER/lib/geronimo-jaxws_2.1_spec-1.0.jar:\
 $EXO_SERVER/lib/geronimo-ws-metadata_2.0_spec-1.1.2.jar:\
 $EXO_SERVER/lib/annotations-api.jar:\
