6923104a0d2c9bcb06d8f76eba5a6a73de87bd9a
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2009-04-14 15:17:01 +0000

    change trunk to version 2.0.2-SNAPSHOT

--- rest/ejb/connector2_1/run.sh
+++ rest/ejb/connector2_1/run.sh
@@ -7,10 +7,10 @@ fi
 
 $JAVA_HOME/bin/java -Djava.security.manager=default  \
 -Djava.security.policy=$JONAS_ROOT/conf/java.policy \
--cp $JONAS_ROOT/lib/apps/exo.core.component.security.core-2.2.jar:\
-$JONAS_ROOT/lib/apps/exo.ws.commons-2.0.1-SNAPSHOT.jar:\
-$JONAS_ROOT/lib/apps/exo.ws.rest.core-2.0.1-SNAPSHOT.jar:\
-$JONAS_ROOT/lib/apps/exo.ws.rest.ext-2.0.1-SNAPSHOT.jar:\
+-cp $JONAS_ROOT/lib/apps/exo.core.component.security.core-2.2.2-SNAPSHOT.jar:\
+$JONAS_ROOT/lib/apps/exo.ws.commons-2.0.2-SNAPSHOT.jar:\
+$JONAS_ROOT/lib/apps/exo.ws.rest.core-2.0.2-SNAPSHOT.jar:\
+$JONAS_ROOT/lib/apps/exo.ws.rest.ext-2.0.2-SNAPSHOT.jar:\
 $JONAS_ROOT/lib/apps/jsr311-api-1.0.jar:\
 $JONAS_ROOT/lib/client.jar \
 org.objectweb.jonas.client.ClientContainer rest-ejb-connector-2_1.ear
